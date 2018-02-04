ce0e5c2748abb1605e9f403beb534d1cc0ee561e
baioburori <4chome7@gmail.com>
2018-02-04 15:41:34 +0900

    index.phpに処理書いちゃった

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-echo 'Hello World!';
-?>
+<html>
+<head>
+<script>
+var execute = function(){
+  var interval = 1000;
+  var exportUser = 1;
+  var importUser = 0;
+    var exportWin = window.open("https://calendar.google.com/calendar/b/" + exportUser + "/exporticalzip");
+    setTimeout(function(){
+      var importWin = window.open("https://calendar.google.com/calendar/b/" + importUser + "/r/settings/export");
+      exportWin.close();
+    },interval);
+}
+
+</script>
+</head>
+<body>
+  <h1>Export Import</h1>
+  <button onclick="execute()">実行</button>
+</body>
+</html
